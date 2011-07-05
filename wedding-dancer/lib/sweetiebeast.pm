79f8f04b7a2c4ada632cc404c5cbb514cb8a90ce
Andrew Grangaard <granny-github@ofb.net>
2011-07-05 11:06:25 -0700

    initial static pages, passes uri_for vars explicitly

--- wedding-dancer/lib/sweetiebeast.pm
+++ wedding-dancer/lib/sweetiebeast.pm
@@ -1,12 +1,21 @@
 package sweetiebeast;
 use Dancer ':syntax';
+#use Dancer::Plugin::ProxyPath;
 
-our $VERSION = '0.1';
+our $VERSION = '0.2';
 
 set auto_page => 1;
+get '/' => sub { template 'index'; };
 
-get '/' => sub {
-    template 'index';
+before_template sub {
+    my $tokens = shift;
+    $tokens->{uri_base}           = request->base->path;
+    $tokens->{uri_for_directions} = request->uri_for("/directions");
+    $tokens->{uri_for_faq}        = request->uri_for("/faq");
+    $tokens->{uri_for_index}      = request->uri_for("/");
+    $tokens->{uri_for_location}   = request->uri_for("/location");
+    $tokens->{uri_for_travel}     = request->uri_for("/travel");
 };
+
 
 true;
